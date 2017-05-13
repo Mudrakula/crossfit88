dd244c20dbc1ca4cd9acb29ebe84a19066289ac0
Artem Mudrak <mavpa_skm@mail.ru>
2017-05-13 17:26:35 +0300

    Calendar. Change months.

--- app/trainers/trainer.controller.js
+++ app/trainers/trainer.controller.js
@@ -6,39 +6,55 @@ angular.module('crossfit88App')
     $scope.weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     $scope.trainings = {};
     $scope.selectedDay = moment();
+    $scope.currentMonth = moment();
     $scope.dayTrainings = [];
+    $scope.monthLabels = {};
 
     $scope.getTrainings = date => {
       return _.filter($scope.trainer.trainings, training => moment(+training.date).isSame(moment(+date), 'day'));
     };
 
     $scope.getMonth = date => {
+      let month = [];
       let days = date.daysInMonth();
       let weeks = Math.ceil(days / 7);
-      let firstDay = date.startOf('month').startOf('isoweek');
-      let firstDayNumber = firstDay.format('d');
-      let month = [];
+      let firstDay = moment(date.format('x'), 'x').startOf('month');
+
+      if (days + firstDay.isoWeekday() - 1 > weeks * 7)
+        weeks++;
+
+      firstDay.startOf('isoweek');
       for (let i = 0; i < weeks; i++) {
         month[i] = [];
         for (let j = 0; j < 7; j++) {
-          let tmp = moment(firstDay.toDate());
+          let tmp = moment(firstDay.format('x'), 'x');
           month[i][j] = tmp.add(j, 'd').format('x');
           $scope.trainings[month[i][j]] = $scope.getTrainings(month[i][j]);
         }
         firstDay.add(1, 'w').startOf('isoweek');
-        firstDayNumber = firstDay.format('d');
       }
 
       $scope.dayTrainings = $scope.trainings[date.startOf('day').format('x')];
+      let tmpDate = moment(date.format('x'), 'x');
+      $scope.monthLabels = {
+        prev: tmpDate.add(-1, 'months').format('MMMM'),
+        next: tmpDate.add(2, 'months').format('MMMM')
+      };
 
       return month;
     };
-    $scope.month = $scope.getMonth(moment());
+    $scope.month = $scope.getMonth($scope.currentMonth);
+
+    $scope.changeMonth = direction => {
+      $scope.currentMonth.add(direction, 'months');
+      $scope.month = $scope.getMonth($scope.currentMonth);
+    }
 
     $scope.checkDay = date => {
       let day = moment(+date);
+      let month = $scope.currentMonth || moment();
       return {
-        disabled: ! day.isSame(moment(), 'month'),
+        disabled: ! day.isSame($scope.currentMonth, 'month'),
         selected: day.isSame($scope.selectedDay, 'day'),
         clickable: $scope.trainings[date].length
       };
